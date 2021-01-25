29ffade68b4bf7d7bbde1b901c1ef83ef826dda6
shibryan28 <77139378+shibryan28@users.noreply.github.com>
2021-01-25 18:02:14 -0500

    Add files via upload

---
+++ JS Course/DOM API/script13.js
@@ -0,0 +1,14 @@
+// 1. Select
+let sign = document.getElementById('sign');
+console.log(sign);
+
+// 2. Modify
+console.log(sign.textContent);
+// modify text
+sign.textContent = 'Welcome, travelers!';
+
+// modify
+// sign.innerHTML = sign.innerHTML + '<p>hello</p>';
+
+// 3. change style
+sign.style.color = 'blue';
